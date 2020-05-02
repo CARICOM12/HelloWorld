cb9643a5ac9576661a44bdbec959379dfb614bef
CARICOM12 <0190a@ukr.net>
2020-05-02 22:10:45 +0300

    Add files via upload

---
+++ HelloWorld/HelloWorld/Board.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Board
+    {
+        public Board()
+        {
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var rand = new Random();
+            Console.WriteLine("Hello World! " + rand.);
+
+        }
+    }
+}
